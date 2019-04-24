7caa8e14eef1d289cf44747c2485b874fc5fbb7b
Alicia Toshima <toshimaalicia@gmail.com>
2019-04-24 00:29:25 -0700

    solved the exercise

--- 8-5.py
+++ 8-5.py
@@ -2,9 +2,9 @@ mbox = open('mbox-short.txt')
 count = 0
 for line in mbox:
     this_line = line.split()
-    print('nemo')
+    if len(this_line) <1:
+        continue
     if this_line[0] == 'From:':
         print(this_line[1])
         count = count + 1
-        print('loach')
 print(count)

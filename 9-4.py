935e7e2d3dcd13bcedbf816904a1512f596f8896
Alicia Toshima <toshimaalicia@gmail.com>
2019-04-30 01:11:35 -0700

    actually finished this exercies. kinda surprised actually

---
+++ 9-4.py
@@ -0,0 +1,22 @@
+# Write a program to read through a mail log, build a histogram using a dictionary to count how many messages have come grom each email address, and print the dictionary.
+
+# basically exercise 8-5 to start
+mbox = open('mbox-short.txt')
+list = []
+dict = dict()
+for line in mbox:
+    this_line = line.split()
+    if len(this_line) <1:
+        continue
+    if this_line[0] == 'From:':
+        list.append(this_line[1])
+
+for email in list:
+    dict[email] = dict.get(email,0) +1
+
+max = None
+for k,v in dict.items():
+    if max == None or max < v:
+        max = v
+        max_key = k
+print(max_key, max)

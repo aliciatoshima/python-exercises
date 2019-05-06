952564bd73b3098e7fa439fd5f997686eaee27f5
Alicia Toshima <toshimaalicia@gmail.com>
2019-05-05 21:59:25 -0700

    one of the exercises to the cs 162 class

---
+++ sum_of_an_array.py
@@ -0,0 +1,21 @@
+# Using a loop, sum all of the whole numbers stored in an array or list (assume there are “length” numbers stored in the array)
+
+array = [1,2,3,4,5]
+
+def my_sum(array):
+    cur_sum = 0
+    for i in array:
+        cur_sum = i + cur_sum
+    return cur_sum
+
+print(my_sum(array))
+
+array2 = [5,1,2,3,4,5]
+
+def my_sum_2(array):
+    curr_sum = 0
+    for i in array[1:]:
+        curr_sum = i + curr_sum
+    return curr_sum
+
+print(my_sum_2(array2))
